96c144f469fa34e118e4d4fe1b922cd1164baf0e
Karyna <karynachmeliuk@gmail.com>
2024-01-03 19:05:10 +0200

    -

--- app/views.py
+++ app/views.py
@@ -256,8 +256,7 @@ def register(request):
 @login_required(login_url='login')
 def account(request, user_id):
     user = get_object_or_404(User, id=user_id)
-    print(user.id)
-    profile = get_object_or_404(Profile, user=user)
+    profile = get_object_or_404(Profile, user.id=user.id)
     print(profile)
 
     created_projects = profile.created_projects.all()
