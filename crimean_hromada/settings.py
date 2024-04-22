5e4ce64daec54cdec530fc6ad1fcd288f30e84f2
hromadianka <99715792+hromadianka@users.noreply.github.com>
2024-04-22 19:29:38 +0200

    Update settings.py

--- crimean_hromada/settings.py
+++ crimean_hromada/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -148,6 +150,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+django_heroku.settings(locals())
+del DATABASES['default']['OPTIONS']['sslmode']
 
 
 # Default primary key field type
